e599ee2acc7ee3f6b87bcf222bcdf88bcadbaf76
swati <swati.goyal@swatis-MacBook-Pro-2.local>
2020-05-14 12:38:53 +0530

    Added clickedExit function and enabled logout on first load

--- static/index.js
+++ static/index.js
@@ -5,6 +5,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const signIn = document.querySelector('#myBtn');
     const logoutButton = document.querySelector('#logout')
 
+    if (localStorage.getItem('username').length > 0) {
+        document.querySelector('.navbar-text').innerHTML = `Welcome, ` + localStorage.getItem('username');
+        signIn.disabled = true;
+        logoutButton.disabled = false;
+        clickedExit();
+    } else {
+        $(document).ready(function () {
+            signIn.disabled = false;
+            logoutButton.disabled = true;
+            clickedSignIn();
+        });
+    }
+
     function clickedSignIn() {
         $('#myBtn').click(function () {
             $("#exampleModalCenter").modal();
@@ -13,25 +26,16 @@ document.addEventListener('DOMContentLoaded', () => {
                 localStorage.setItem('username', $('#username').val());
                 signIn.disabled = true;
                 logoutButton.disabled = false;
+                clickedExit();
             });
         });
     };
 
-    $('#logout').click(function () {
-        $('.navbar-text').text("Logged out");
-        localStorage.removeItem('username');
-        //click on sign-in to join again!
-        signIn.disabled = false;
-        logoutButton.disabled = true;
-        clickedSignIn();
-    });
-
-    if (localStorage.getItem('username').length > 0) {
-        document.querySelector('.navbar-text').innerHTML = `Welcome, ` + localStorage.getItem('username');
-        signIn.disabled = true;
-        logoutButton.disabled = false;
-    } else {
-        $(document).ready(function () {
+    function clickedExit() {
+        $('#logout').click(function () {
+            $('.navbar-text').text("Logged out");
+            localStorage.removeItem('username');
+            //click on sign-in to join again!
             signIn.disabled = false;
             logoutButton.disabled = true;
             clickedSignIn();
