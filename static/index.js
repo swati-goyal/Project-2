eea6020b4290f9ab22e053b021037ed65ca190db
swati <swati.goyal@swatis-MacBook-Pro-2.local>
2020-05-14 12:20:35 +0530

    Finished exit functionality

--- static/index.js
+++ static/index.js
@@ -2,22 +2,39 @@ if (!localStorage.getItem('username'))
     localStorage.setItem('username', "");
 
 document.addEventListener('DOMContentLoaded', () => {
-    const signIn = document.querySelector('#myBtn')
+    const signIn = document.querySelector('#myBtn');
+    const logoutButton = document.querySelector('#logout')
+
+    function clickedSignIn() {
+        $('#myBtn').click(function () {
+            $("#exampleModalCenter").modal();
+            $('#save').on('click', function () {
+                $('.navbar-text').text(`Welcome, ` + $('#username').val());
+                localStorage.setItem('username', $('#username').val());
+                signIn.disabled = true;
+                logoutButton.disabled = false;
+            });
+        });
+    };
+
+    $('#logout').click(function () {
+        $('.navbar-text').text("Logged out");
+        localStorage.removeItem('username');
+        //click on sign-in to join again!
+        signIn.disabled = false;
+        logoutButton.disabled = true;
+        clickedSignIn();
+    });
 
     if (localStorage.getItem('username').length > 0) {
-        document.querySelector('.navbar-text').innerHTML = `Logged in user: ` + localStorage.getItem('username');
+        document.querySelector('.navbar-text').innerHTML = `Welcome, ` + localStorage.getItem('username');
         signIn.disabled = true;
+        logoutButton.disabled = false;
     } else {
         $(document).ready(function () {
             signIn.disabled = false;
-            $('#myBtn').click(function () {
-                $("#exampleModalCenter").modal();
-                $('#save').on('click', function () {
-                    $('.navbar-text').text(`Logged in user: ` + $('#username').val());
-                    localStorage.setItem('username', $('#username').val());
-                    signIn.disabled = true;
-                });
-            });
+            logoutButton.disabled = true;
+            clickedSignIn();
         });
     }
 });
